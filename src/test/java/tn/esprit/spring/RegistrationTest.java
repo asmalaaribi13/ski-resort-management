9598015a4a32a13dd44172b156c7c4d909c230e6
cheima douiss <douisscheima4@gmail.com>
2024-10-11 20:07:26 +0200

    tests commit

--- src/test/java/tn/esprit/spring/RegistrationTest.java
+++ src/test/java/tn/esprit/spring/RegistrationTest.java
@@ -1,2 +1,71 @@
-package tn.esprit.spring;public class RegistrationTest {
+package tn.esprit.spring;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.Registration;
+import tn.esprit.spring.entities.Skier;
+import tn.esprit.spring.entities.TypeCourse;
+import tn.esprit.spring.repositories.ICourseRepository;
+import tn.esprit.spring.repositories.IRegistrationRepository;
+import tn.esprit.spring.repositories.ISkierRepository;
+import tn.esprit.spring.services.RegistrationServicesImpl;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+
+
+import java.util.Optional;
+
+public class RegistrationTest {
+    @InjectMocks
+    private RegistrationServicesImpl registrationServices;
+    @Mock
+    private IRegistrationRepository registrationRepository;
+    @Mock
+    private ISkierRepository skierRepository;
+    @Mock
+    private ICourseRepository courseRepository;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+    @Test
+    void testAddRegistrationAndAssignToSkier() {
+        Registration registration = new Registration();
+        Skier skier = new Skier();
+        skier.setNumSkier(1L);
+        when(skierRepository.findById(1L)).thenReturn(Optional.of(skier));
+        when(registrationRepository.save(any(Registration.class))).thenReturn(registration);
+
+        Registration result = registrationServices.addRegistrationAndAssignToSkier(registration, 1L);
+        assertNotNull(result);
+        assertEquals(skier, result.getSkier());
+        verify(registrationRepository, times(1)).save(registration);
+    }
+
+    @Test
+    void testAddRegistrationAndAssignToSkierAndCourse() {
+        Registration registration = new Registration();
+        Skier skier = new Skier();
+        skier.setNumSkier(1L);
+        Course course = new Course();
+        course.setNumCourse(1L);
+        course.setTypeCourse(TypeCourse.COLLECTIVE_CHILDREN);
+
+        when(skierRepository.findById(1L)).thenReturn(Optional.of(skier));
+        when(courseRepository.findById(1L)).thenReturn(Optional.of(course));
+        when(registrationRepository.countDistinctByNumWeekAndSkier_NumSkierAndCourse_NumCourse(1, 1L, 1L)).thenReturn(0L);
+        when(registrationRepository.save(any(Registration.class))).thenReturn(registration);
+        Registration result = registrationServices.addRegistrationAndAssignToSkierAndCourse(registration, 1L, 1L);
+
+        assertNotNull(result);
+        verify(registrationRepository, times(1)).save(registration);
+    }
+
 }
